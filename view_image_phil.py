d856e433728c57ebaca0d5a16195b7a50096f881
filipposkar <62504405+filipposkar@users.noreply.github.com>
2022-04-17 22:13:40 +0300

    summary

--- view_image_phil.py
+++ view_image_phil.py
@@ -1,6 +1,8 @@
 # Let's visualize our images
 
-# view_image_phil("/content/drive/MyDrive/data","phil_pred.jpg" )
+# view_image_phil("/content/drive/MyDrive/data","phil_pred.jpg")
+# !wget https://github.com/filipposkar/phil/blob/main/maria.jpg?raw=true -O maria.jpg
+# view_image_phil("/content", "maria.jpg")
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
