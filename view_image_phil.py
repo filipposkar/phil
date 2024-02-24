c0f0982b7a9d78923168da25226294bc4d99ffd2
filipposkar <62504405+filipposkar@users.noreply.github.com>
2024-02-24 18:16:55 +0200

    Update view_image_phil.py

--- view_image_phil.py
+++ view_image_phil.py
@@ -19,10 +19,7 @@ def view_image_phil(target_dir, target_image):
 
   # Read in the image and plot it using matplotlib
   img = mpimg.imread(target_dir + "/" + target_image)
-  plt.imshow(img)
+  plt.imshow(img);
   #plt.axis("off")
 
-  bool = False
-  bool = True
-
-  return bool
+  return True
