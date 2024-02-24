f2ccaab17788310cb28d0daf5809d06081090ec5
filipposkar <62504405+filipposkar@users.noreply.github.com>
2024-02-24 17:59:26 +0200

    Update view_image_phil.py

--- view_image_phil.py
+++ view_image_phil.py
@@ -4,6 +4,13 @@
 # !wget https://github.com/filipposkar/phil/blob/main/maria.jpg?raw=true -O maria.jpg
 # view_image_phil("/content", "maria.jpg")
 
+'''
+this function shows a =n image stored in a folder
+input:
+  target_dir:   the target folder
+  target_image: the target image (the image to show)
+'''
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import random
