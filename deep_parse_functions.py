3f4276a48f76a56e5f43d53f533088b2a99d74f8
filipposkar <62504405+filipposkar@users.noreply.github.com>
2024-04-28 18:25:45 +0300

    Update deep_parse_functions.py

--- deep_parse_functions.py
+++ deep_parse_functions.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+# Convert address to NGRAM
 def convert_word_to_ngram(word, ngram):
   """
   converts a word to ngrams
@@ -18,8 +20,42 @@ def convert_word_to_ngram(word, ngram):
 
   return "".join((x) for x in word_list)
 
+# Standarize address 1
+# Strip accents and lowercase
 import unicodedata
+
 def strip_accents_and_lowercase(s):
    return ''.join(c for c in unicodedata.normalize('NFD', s)
                   if unicodedata.category(c) != 'Mn').lower()
+
+# Convert address to model input
+def convert_address_to_model_input(address, 
+                                   standarize_bool=False, 
+                                   ngram_bool=False, 
+                                   char_bool=False):
+  """
+  accepts as input an address string and converts it to model input format for prediction
+  it is better to replace char_embed_bool to model name (model_1, etc.)
+  """
+
+  import unicodedata
+
+  if standarize_bool:
+    address = strip_accents_and_lowercase(address)
+
+  # models: baseline, conv1d
+  formated_address = address.split()
+
+  # models: ngram
+  if ngram_bool:
+    formated_address = convert_word_to_ngram(address, 3)
+
+  # models: char embed
+  if char_bool:
+    formated_address_list = []
+    for element in formated_address:
+      formated_address_list.append(split_chars(element))
+    formated_address = formated_address_list
+
+  return formated_address
 
