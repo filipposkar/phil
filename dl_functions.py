f6530665394549af9aaef24dbeab732fd938834a
filipposkar <62504405+filipposkar@users.noreply.github.com>
2024-12-17 17:45:16 +0200

    Update dl_functions.py

--- dl_functions.py
+++ dl_functions.py
@@ -8,7 +8,27 @@ Original file is located at
 """
 
 # deep parse functions
+import numpy as np
 
+# Convert address to NGRAM
+def convert_word_to_ngram(word, ngram):
+  """
+  converts a word to ngrams
+  """
+  import numpy as np
+  
+  word_list = []
+
+  word_chars = list(word)
+  word_len = len(word_chars)
+
+  for i in np.arange(1, word_len, 1):
+    word_list.append(word_chars[i-1])
+    if i % ngram == 0:
+      word_list.append(" ")
+  word_list.append(word_chars[word_len-1])
+
+  return "".join((x) for x in word_list)
 
 # Make function to split sentences into characters
 def split_chars(text):
