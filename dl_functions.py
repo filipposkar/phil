03b7b8566e62de747ee69ef6ed220439565586c9
filipposkar <62504405+filipposkar@users.noreply.github.com>
2025-05-16 21:20:35 +0300

    Update dl_functions.py

--- dl_functions.py
+++ dl_functions.py
@@ -73,6 +73,9 @@ def convert_address_to_model_input_2(address, model_name):
 
   if model_name == "model_700" or model_name == "model_7": # char n-gram (OK)
     converted_address = [tf.constant(address_char), tf.constant(address_ngram)]
+
+  if model_name == "model_800" or model_name == "model_8": # deep_parse_v1 (OK)
+    converted_address = [(tf.constant(address_char), tf.constant(address_ngram)), tf.constant(address_split)]
 	  
   if model_name == "model_200" or model_name == "model_2": # conv1d_positional_embeddings (OK)
 
